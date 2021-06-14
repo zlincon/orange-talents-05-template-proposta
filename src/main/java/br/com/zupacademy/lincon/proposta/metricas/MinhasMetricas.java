9ea68f9febed543e29cd066fc4e12b179d408ada
DELLWorks <linconsilva08@gmail.com>
2021-06-14 09:25:47 -0300

    Prometheus - set up

---
+++ src/main/java/br/com/zupacademy/lincon/proposta/metricas/MinhasMetricas.java
@@ -0,0 +1,30 @@
+package br.com.zupacademy.lincon.proposta.metricas;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+class MinhasMetricas {
+
+    private final MeterRegistry meterRegistry;
+
+    public MinhasMetricas(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    public void meuContador() {
+        Collection<Tag> tags = new ArrayList<>();
+        tags.add(Tag.of("emissora", "Mastercard"));
+        tags.add(Tag.of("banco", "Itaú"));
+
+        Counter contadorDePropostasCriadas = this.meterRegistry.counter("proposta_criada", tags);
+
+        contadorDePropostasCriadas.increment();
+    }
+}
+
